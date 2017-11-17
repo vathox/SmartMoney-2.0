b338facf566cb183563d495920ace8be591f42ea
Vathox <dzierzko.a@gmail.com>
2017-11-17 15:08:54 -0500

    Separate the styles into own file, make the build more development friendly

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from '../actions/filters'
 import {DateRangePicker} from 'react-dates'
+import 'react-dates/initialize'
 
 class ExpenseListFilters extends React.Component{
     state= {
